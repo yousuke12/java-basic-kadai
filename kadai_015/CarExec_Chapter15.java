3f89aa10b3d77aff78172ff5832e0c6185142307
yousuke12 <nakagawa_y@digicomnet.co.jp>
2026-05-21 11:21:26 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,5 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+}

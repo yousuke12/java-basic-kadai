2da077faee3d45973e13ecb53badac1493aa8966
yousuke12 <nakagawa_y@digicomnet.co.jp>
2026-05-21 11:34:26 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -4,7 +4,16 @@ public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		// 1. 車クラスのインスタンスを作成する
+        Car_Chapter15 myCar = new Car_Chapter15();
+        
+        // 2. 車クラスのchangeGearメソッドを実行する（引数として3を渡す）
+        myCar.changeGear(3);
+        
+        // 3. 車クラスのrunメソッドを実行する
+        myCar.run();
+        
+    }
 	}
 
-}
+
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,5 +1,31 @@
 package kadai_015;
 
 public class Car_Chapter15 {
+	private int gear = 1;  // 1速から5速のギアを表す（初期値1）
+    private int speed = 10; // ギアチェンジ後の速度を表す（初期値10）
 
+    // コンストラクタ（今回は特に処理なしでOK）
+    public Car_Chapter15() {
+    }
+
+    // メソッド：ギアの値により速度を変える
+    public void changeGear(int afterGear) {
+        System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+        this.gear = afterGear;
+
+        // ギアに応じた速度をswitch文で判定
+        switch (this.gear) {
+            case 1 -> this.speed = 10;
+            case 2 -> this.speed = 20;
+            case 3 -> this.speed = 30;
+            case 4 -> this.speed = 40;
+            case 5 -> this.speed = 50;
+            default -> this.speed = 10; // 上記以外のギアの場合
+        }
+    }
+
+    // メソッド：ギアチェンジ後の速度を表示する
+    public void run() {
+        System.out.println("速度は時速" + this.speed + "kmです");
+    }
 }
